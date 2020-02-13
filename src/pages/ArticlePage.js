e438320622bd4966d2d04f7b8d0727f88c501f26
Joseph_Tully <tully4school@gmail.com>
2020-02-13 15:54:43 -0500

    making upvotes visible and malleable using react hooks usestate and useeffect

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,25 +1,34 @@
-import React from 'react'
-import ArticlesList from '../Components/ArticlesList'
-import NotFoundPage from './NotFoundPage'
-import articleContent from './article-content'
+import React, { useState, useEffect } from "react";
+import ArticlesList from "../Components/ArticlesList";
+import NotFoundPage from "./NotFoundPage";
+import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
-  const name = match.params.name
-  const article = articleContent.find(article => article.name === name)
+	fetch("/api/articles/...");
 
-  if (!article) return <NotFoundPage />
+	const name = match.params.name;
+	const article = articleContent.find(article => article.name === name);
 
-  const otherArticles = articleContent.filter(article => article.name !== name)
+	const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
 
-  return (
-    <>
-      <h1>{article.title}</h1>
-      {article.content.map((paragraph, key) => (
-        <p key={key}>{paragraph}</p>
-      ))}
-      <h3>Other Articles</h3>
-      <ArticlesList articles={otherArticles} />
-    </>
-  )
-}
-export default ArticlePage
+	useEffect(() => {
+		setArticleInfo({ upvotes: 3 });
+	});
+
+	if (!article) return <NotFoundPage />;
+
+	const otherArticles = articleContent.filter(article => article.name !== name);
+
+	return (
+		<>
+			<h1>{article.title}</h1>
+			<p>This post has been upvoted {articleInfo.upvotes} times</p>
+			{article.content.map((paragraph, key) => (
+				<p key={key}>{paragraph}</p>
+			))}
+			<h3>Other Articles</h3>
+			<ArticlesList articles={otherArticles} />
+		</>
+	);
+};
+export default ArticlePage;
