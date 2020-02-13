3843bc371e9669f2ac2baa68d67004b89459f1c2
Joseph_Tully <tully4school@gmail.com>
2020-02-13 07:04:21 -0500

    Created 404 page added a route, and added the component to articlepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css'
 import Homepage from './pages/Homepage'
 import About from './pages/About'
 import ArticlePage from './pages/ArticlePage'
 import ArticlesListPage from './pages/ArticlesListPage'
+import NotFoundPage from './pages/NotFoundPage'
 import NavBar from './NavBar'
 
 function App () {
@@ -13,10 +14,13 @@ function App () {
       <div className='App'>
         <NavBar />
         <div id='page-body'>
-          <Route path='/' exact component={Homepage} />
-          <Route path='/about' exact component={About} />
-          <Route path='/articles-list' exact component={ArticlesListPage} />
-          <Route path='/article/:name' exact component={ArticlePage} />
+          <Switch>
+            <Route path='/' exact component={Homepage} />
+            <Route path='/about' exact component={About} />
+            <Route path='/articles-list' exact component={ArticlesListPage} />
+            <Route path='/article/:name' exact component={ArticlePage} />
+            <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
     </Router>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ArticlesList from '../Components/ArticlesList'
+import NotFoundPage from './NotFoundPage'
 import articleContent from './article-content'
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name
   const article = articleContent.find(article => article.name === name)
 
-  if (!article) return <h1>Article does not exist</h1>
+  if (!article) return <NotFoundPage />
 
   const otherArticles = articleContent.filter(article => article.name !== name)
 
@@ -16,6 +17,7 @@ const ArticlePage = ({ match }) => {
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other Articles</h3>
       <ArticlesList articles={otherArticles} />
     </>
   )
